a15fb4c30eef56cca263af4363992e5cfc61afc3
brenman60 <83514481+brenman60@users.noreply.github.com>
2025-01-06 18:38:43 -0500

    Initial commit

---
+++ Binary Clock/frmBinaryClock.Designer.vb
@@ -0,0 +1,360 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBinaryClock
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.picHour1_1 = New System.Windows.Forms.PictureBox()
+        Me.picHour1_2 = New System.Windows.Forms.PictureBox()
+        Me.picHour2_2 = New System.Windows.Forms.PictureBox()
+        Me.picHour2_1 = New System.Windows.Forms.PictureBox()
+        Me.picHour2_4 = New System.Windows.Forms.PictureBox()
+        Me.picHour2_8 = New System.Windows.Forms.PictureBox()
+        Me.picMinute2_8 = New System.Windows.Forms.PictureBox()
+        Me.picMinute2_4 = New System.Windows.Forms.PictureBox()
+        Me.picMinute2_2 = New System.Windows.Forms.PictureBox()
+        Me.picMinute2_1 = New System.Windows.Forms.PictureBox()
+        Me.picMinute1_2 = New System.Windows.Forms.PictureBox()
+        Me.picMinute1_1 = New System.Windows.Forms.PictureBox()
+        Me.picMinute1_4 = New System.Windows.Forms.PictureBox()
+        Me.picSecond1_4 = New System.Windows.Forms.PictureBox()
+        Me.picSecond1_2 = New System.Windows.Forms.PictureBox()
+        Me.picSecond1_1 = New System.Windows.Forms.PictureBox()
+        Me.picSecond2_8 = New System.Windows.Forms.PictureBox()
+        Me.picSecond2_4 = New System.Windows.Forms.PictureBox()
+        Me.picSecond2_2 = New System.Windows.Forms.PictureBox()
+        Me.picSecond2_1 = New System.Windows.Forms.PictureBox()
+        Me.BinaryTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.lblTitle = New System.Windows.Forms.Label()
+        CType(Me.picHour1_1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picHour1_2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picHour2_2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picHour2_1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picHour2_4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picHour2_8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picMinute2_8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picMinute2_4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picMinute2_2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picMinute2_1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picMinute1_2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picMinute1_1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picMinute1_4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picSecond1_4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picSecond1_2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picSecond1_1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picSecond2_8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picSecond2_4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picSecond2_2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picSecond2_1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picHour1_1
+        '
+        Me.picHour1_1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.picHour1_1.Image = Global.Binary_Clock.My.Resources.Resources.LightOff
+        Me.picHour1_1.Location = New System.Drawing.Point(24, 393)
+        Me.picHour1_1.Name = "picHour1_1"
+        Me.picHour1_1.Size = New System.Drawing.Size(80, 80)
+        Me.picHour1_1.TabIndex = 0
+        Me.picHour1_1.TabStop = False
+        '
+        'picHour1_2
+        '
+        Me.picHour1_2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.picHour1_2.Image = Global.Binary_Clock.My.Resources.Resources.LightOff
+        Me.picHour1_2.Location = New System.Drawing.Point(24, 307)
+        Me.picHour1_2.Name = "picHour1_2"
+        Me.picHour1_2.Size = New System.Drawing.Size(80, 80)
+        Me.picHour1_2.TabIndex = 1
+        Me.picHour1_2.TabStop = False
+        '
+        'picHour2_2
+        '
+        Me.picHour2_2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.picHour2_2.Image = Global.Binary_Clock.My.Resources.Resources.LightOff
+        Me.picHour2_2.Location = New System.Drawing.Point(110, 307)
+        Me.picHour2_2.Name = "picHour2_2"
+        Me.picHour2_2.Size = New System.Drawing.Size(80, 80)
+        Me.picHour2_2.TabIndex = 3
+        Me.picHour2_2.TabStop = False
+        '
+        'picHour2_1
+        '
+        Me.picHour2_1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.picHour2_1.Image = Global.Binary_Clock.My.Resources.Resources.LightOff
+        Me.picHour2_1.Location = New System.Drawing.Point(110, 393)
+        Me.picHour2_1.Name = "picHour2_1"
+        Me.picHour2_1.Size = New System.Drawing.Size(80, 80)
+        Me.picHour2_1.TabIndex = 2
+        Me.picHour2_1.TabStop = False
+        '
+        'picHour2_4
+        '
+        Me.picHour2_4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.picHour2_4.Image = Global.Binary_Clock.My.Resources.Resources.LightOff
+        Me.picHour2_4.Location = New System.Drawing.Point(110, 221)
+        Me.picHour2_4.Name = "picHour2_4"
+        Me.picHour2_4.Size = New System.Drawing.Size(80, 80)
+        Me.picHour2_4.TabIndex = 4
+        Me.picHour2_4.TabStop = False
+        '
+        'picHour2_8
+        '
+        Me.picHour2_8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.picHour2_8.Image = Global.Binary_Clock.My.Resources.Resources.LightOff
+        Me.picHour2_8.Location = New System.Drawing.Point(110, 135)
+        Me.picHour2_8.Name = "picHour2_8"
+        Me.picHour2_8.Size = New System.Drawing.Size(80, 80)
+        Me.picHour2_8.TabIndex = 5
+        Me.picHour2_8.TabStop = False
+        '
+        'picMinute2_8
+        '
+        Me.picMinute2_8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.picMinute2_8.Image = Global.Binary_Clock.My.Resources.Resources.LightOff
+        Me.picMinute2_8.Location = New System.Drawing.Point(282, 135)
+        Me.picMinute2_8.Name = "picMinute2_8"
+        Me.picMinute2_8.Size = New System.Drawing.Size(80, 80)
+        Me.picMinute2_8.TabIndex = 11
+        Me.picMinute2_8.TabStop = False
+        '
+        'picMinute2_4
+        '
+        Me.picMinute2_4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.picMinute2_4.Image = Global.Binary_Clock.My.Resources.Resources.LightOff
+        Me.picMinute2_4.Location = New System.Drawing.Point(282, 221)
+        Me.picMinute2_4.Name = "picMinute2_4"
+        Me.picMinute2_4.Size = New System.Drawing.Size(80, 80)
+        Me.picMinute2_4.TabIndex = 10
+        Me.picMinute2_4.TabStop = False
+        '
+        'picMinute2_2
+        '
+        Me.picMinute2_2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.picMinute2_2.Image = Global.Binary_Clock.My.Resources.Resources.LightOff
+        Me.picMinute2_2.Location = New System.Drawing.Point(282, 307)
+        Me.picMinute2_2.Name = "picMinute2_2"
+        Me.picMinute2_2.Size = New System.Drawing.Size(80, 80)
+        Me.picMinute2_2.TabIndex = 9
+        Me.picMinute2_2.TabStop = False
+        '
+        'picMinute2_1
+        '
+        Me.picMinute2_1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.picMinute2_1.Image = Global.Binary_Clock.My.Resources.Resources.LightOff
+        Me.picMinute2_1.Location = New System.Drawing.Point(282, 393)
+        Me.picMinute2_1.Name = "picMinute2_1"
+        Me.picMinute2_1.Size = New System.Drawing.Size(80, 80)
+        Me.picMinute2_1.TabIndex = 8
+        Me.picMinute2_1.TabStop = False
+        '
+        'picMinute1_2
+        '
+        Me.picMinute1_2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.picMinute1_2.Image = Global.Binary_Clock.My.Resources.Resources.LightOff
+        Me.picMinute1_2.Location = New System.Drawing.Point(196, 307)
+        Me.picMinute1_2.Name = "picMinute1_2"
+        Me.picMinute1_2.Size = New System.Drawing.Size(80, 80)
+        Me.picMinute1_2.TabIndex = 7
+        Me.picMinute1_2.TabStop = False
+        '
+        'picMinute1_1
+        '
+        Me.picMinute1_1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.picMinute1_1.Image = Global.Binary_Clock.My.Resources.Resources.LightOff
+        Me.picMinute1_1.Location = New System.Drawing.Point(196, 393)
+        Me.picMinute1_1.Name = "picMinute1_1"
+        Me.picMinute1_1.Size = New System.Drawing.Size(80, 80)
+        Me.picMinute1_1.TabIndex = 6
+        Me.picMinute1_1.TabStop = False
+        '
+        'picMinute1_4
+        '
+        Me.picMinute1_4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.picMinute1_4.Image = Global.Binary_Clock.My.Resources.Resources.LightOff
+        Me.picMinute1_4.Location = New System.Drawing.Point(196, 221)
+        Me.picMinute1_4.Name = "picMinute1_4"
+        Me.picMinute1_4.Size = New System.Drawing.Size(80, 80)
+        Me.picMinute1_4.TabIndex = 12
+        Me.picMinute1_4.TabStop = False
+        '
+        'picSecond1_4
+        '
+        Me.picSecond1_4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.picSecond1_4.Image = Global.Binary_Clock.My.Resources.Resources.LightOff
+        Me.picSecond1_4.Location = New System.Drawing.Point(368, 221)
+        Me.picSecond1_4.Name = "picSecond1_4"
+        Me.picSecond1_4.Size = New System.Drawing.Size(80, 80)
+        Me.picSecond1_4.TabIndex = 15
+        Me.picSecond1_4.TabStop = False
+        '
+        'picSecond1_2
+        '
+        Me.picSecond1_2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.picSecond1_2.Image = Global.Binary_Clock.My.Resources.Resources.LightOff
+        Me.picSecond1_2.Location = New System.Drawing.Point(368, 307)
+        Me.picSecond1_2.Name = "picSecond1_2"
+        Me.picSecond1_2.Size = New System.Drawing.Size(80, 80)
+        Me.picSecond1_2.TabIndex = 14
+        Me.picSecond1_2.TabStop = False
+        '
+        'picSecond1_1
+        '
+        Me.picSecond1_1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.picSecond1_1.Image = Global.Binary_Clock.My.Resources.Resources.LightOff
+        Me.picSecond1_1.Location = New System.Drawing.Point(368, 393)
+        Me.picSecond1_1.Name = "picSecond1_1"
+        Me.picSecond1_1.Size = New System.Drawing.Size(80, 80)
+        Me.picSecond1_1.TabIndex = 13
+        Me.picSecond1_1.TabStop = False
+        '
+        'picSecond2_8
+        '
+        Me.picSecond2_8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.picSecond2_8.Image = Global.Binary_Clock.My.Resources.Resources.LightOff
+        Me.picSecond2_8.Location = New System.Drawing.Point(454, 135)
+        Me.picSecond2_8.Name = "picSecond2_8"
+        Me.picSecond2_8.Size = New System.Drawing.Size(80, 80)
+        Me.picSecond2_8.TabIndex = 19
+        Me.picSecond2_8.TabStop = False
+        '
+        'picSecond2_4
+        '
+        Me.picSecond2_4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.picSecond2_4.Image = Global.Binary_Clock.My.Resources.Resources.LightOff
+        Me.picSecond2_4.Location = New System.Drawing.Point(454, 221)
+        Me.picSecond2_4.Name = "picSecond2_4"
+        Me.picSecond2_4.Size = New System.Drawing.Size(80, 80)
+        Me.picSecond2_4.TabIndex = 18
+        Me.picSecond2_4.TabStop = False
+        '
+        'picSecond2_2
+        '
+        Me.picSecond2_2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.picSecond2_2.Image = Global.Binary_Clock.My.Resources.Resources.LightOff
+        Me.picSecond2_2.Location = New System.Drawing.Point(454, 307)
+        Me.picSecond2_2.Name = "picSecond2_2"
+        Me.picSecond2_2.Size = New System.Drawing.Size(80, 80)
+        Me.picSecond2_2.TabIndex = 17
+        Me.picSecond2_2.TabStop = False
+        '
+        'picSecond2_1
+        '
+        Me.picSecond2_1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.picSecond2_1.Image = Global.Binary_Clock.My.Resources.Resources.LightOff
+        Me.picSecond2_1.Location = New System.Drawing.Point(454, 393)
+        Me.picSecond2_1.Name = "picSecond2_1"
+        Me.picSecond2_1.Size = New System.Drawing.Size(80, 80)
+        Me.picSecond2_1.TabIndex = 16
+        Me.picSecond2_1.TabStop = False
+        '
+        'BinaryTimer
+        '
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Leelawadee", 32.0!, System.Drawing.FontStyle.Bold)
+        Me.lblTitle.Location = New System.Drawing.Point(141, 41)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(277, 51)
+        Me.lblTitle.TabIndex = 20
+        Me.lblTitle.Text = "Binary Clock"
+        '
+        'frmBinaryClock
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.ClientSize = New System.Drawing.Size(559, 482)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.picSecond2_8)
+        Me.Controls.Add(Me.picSecond2_4)
+        Me.Controls.Add(Me.picSecond2_2)
+        Me.Controls.Add(Me.picSecond2_1)
+        Me.Controls.Add(Me.picSecond1_4)
+        Me.Controls.Add(Me.picSecond1_2)
+        Me.Controls.Add(Me.picSecond1_1)
+        Me.Controls.Add(Me.picMinute1_4)
+        Me.Controls.Add(Me.picMinute2_8)
+        Me.Controls.Add(Me.picMinute2_4)
+        Me.Controls.Add(Me.picMinute2_2)
+        Me.Controls.Add(Me.picMinute2_1)
+        Me.Controls.Add(Me.picMinute1_2)
+        Me.Controls.Add(Me.picMinute1_1)
+        Me.Controls.Add(Me.picHour2_8)
+        Me.Controls.Add(Me.picHour2_4)
+        Me.Controls.Add(Me.picHour2_2)
+        Me.Controls.Add(Me.picHour2_1)
+        Me.Controls.Add(Me.picHour1_2)
+        Me.Controls.Add(Me.picHour1_1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "frmBinaryClock"
+        Me.Text = "Binary Clock"
+        CType(Me.picHour1_1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picHour1_2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picHour2_2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picHour2_1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picHour2_4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picHour2_8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picMinute2_8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picMinute2_4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picMinute2_2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picMinute2_1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picMinute1_2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picMinute1_1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picMinute1_4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picSecond1_4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picSecond1_2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picSecond1_1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picSecond2_8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picSecond2_4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picSecond2_2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picSecond2_1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picHour1_1 As PictureBox
+    Friend WithEvents picHour1_2 As PictureBox
+    Friend WithEvents picHour2_2 As PictureBox
+    Friend WithEvents picHour2_1 As PictureBox
+    Friend WithEvents picHour2_4 As PictureBox
+    Friend WithEvents picHour2_8 As PictureBox
+    Friend WithEvents picMinute2_8 As PictureBox
+    Friend WithEvents picMinute2_4 As PictureBox
+    Friend WithEvents picMinute2_2 As PictureBox
+    Friend WithEvents picMinute2_1 As PictureBox
+    Friend WithEvents picMinute1_2 As PictureBox
+    Friend WithEvents picMinute1_1 As PictureBox
+    Friend WithEvents picMinute1_4 As PictureBox
+    Friend WithEvents picSecond1_4 As PictureBox
+    Friend WithEvents picSecond1_2 As PictureBox
+    Friend WithEvents picSecond1_1 As PictureBox
+    Friend WithEvents picSecond2_8 As PictureBox
+    Friend WithEvents picSecond2_4 As PictureBox
+    Friend WithEvents picSecond2_2 As PictureBox
+    Friend WithEvents picSecond2_1 As PictureBox
+    Friend WithEvents BinaryTimer As Timer
+    Friend WithEvents lblTitle As Label
+End Class
---
+++ Binary Clock/frmBinaryClock.vb
@@ -0,0 +1,119 @@
+﻿'Developer Name: Brennan Kunicki
+'Program Name: Binary Clock
+'Date: 12/12/24
+'Purpose of Program: Shows a binary representation of the current time.
+Public Class frmBinaryClock
+    Const _cintTimerSpeed = 500
+
+    Private Sub frmBinaryClock_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Begins timer control
+        BinaryTimer.Interval = _cintTimerSpeed
+        BinaryTimer.Start()
+    End Sub
+
+    Private Sub BinaryTimer_Tick(sender As Object, e As EventArgs) Handles BinaryTimer.Tick
+        'Updates all timer images through their respective sub procedures
+        Dim intHour1 As Integer = Now.Hour.ToString("D2").Substring(0, 1)
+        Dim intHour2 As Integer = Now.Hour.ToString("D2").Substring(1, 1)
+
+        Dim intMinute1 As Integer = Now.Minute.ToString("D2").Substring(0, 1)
+        Dim intMinute2 As Integer = Now.Minute.ToString("D2").Substring(1, 1)
+
+        Dim intSecond1 As Integer = Now.Second.ToString("D2").Substring(0, 1)
+        Dim intSecond2 As Integer = Now.Second.ToString("D2").Substring(1, 1)
+
+        TimerHourUpdate(intHour1, intHour2)
+        TimerMinuteUpdate(intMinute1, intMinute2)
+        TimerSecondUpdate(intSecond1, intSecond2)
+    End Sub
+
+    Private Sub TimerHourUpdate(ByRef hour1 As Integer, ByRef hour2 As Integer)
+        'Updates the hour PictureBoxes
+        Dim arrHours1 As PictureBox() = {picHour1_1, picHour1_2}
+        Dim arrHours2 As PictureBox() = {picHour2_1, picHour2_2, picHour2_4, picHour2_8}
+        Array.Reverse(arrHours1)
+        Array.Reverse(arrHours2)
+
+        'Updates the PictureBoxes representing the ones place
+        For Each time1 As PictureBox In arrHours1
+            Dim time As Integer = Convert.ToInt16(time1.Name.Substring(time1.Name.Length - 1, 1))
+            If time <= hour1 Then
+                time1.Image = My.Resources.LightOn
+                hour1 -= time
+            Else
+                time1.Image = My.Resources.LightOff
+            End If
+        Next
+
+        'Updates the PictureBoxes representing the tens place
+        For Each time2 As PictureBox In arrHours2
+            Dim time As Integer = Convert.ToInt16(time2.Name.Substring(time2.Name.Length - 1, 1))
+            If time <= hour2 Then
+                time2.Image = My.Resources.LightOn
+                hour2 -= time
+            Else
+                time2.Image = My.Resources.LightOff
+            End If
+        Next
+    End Sub
+
+    Private Sub TimerMinuteUpdate(ByRef minute1 As Integer, ByRef minute2 As Integer)
+        'Updates the minute PictureBoxes
+        Dim arrMinutes1 As PictureBox() = {picMinute1_1, picMinute1_2, picMinute1_4}
+        Dim arrMinutes2 As PictureBox() = {picMinute2_1, picMinute2_2, picMinute2_4, picMinute2_8}
+        Array.Reverse(arrMinutes1)
+        Array.Reverse(arrMinutes2)
+
+        'Updates the PictureBoxes representing the ones place
+        For Each time1 As PictureBox In arrMinutes1
+            Dim time As Integer = Convert.ToInt16(time1.Name.Substring(time1.Name.Length - 1, 1))
+            If time <= minute1 Then
+                time1.Image = My.Resources.LightOn
+                minute1 -= time
+            Else
+                time1.Image = My.Resources.LightOff
+            End If
+        Next
+
+        'Updates the PictureBoxes representing the tens place
+        For Each time2 As PictureBox In arrMinutes2
+            Dim time As Integer = Convert.ToInt16(time2.Name.Substring(time2.Name.Length - 1, 1))
+            If time <= minute2 Then
+                time2.Image = My.Resources.LightOn
+                minute2 -= time
+            Else
+                time2.Image = My.Resources.LightOff
+            End If
+        Next
+    End Sub
+
+    Private Sub TimerSecondUpdate(ByRef second1 As Integer, ByRef second2 As Integer)
+        'Updates the second PictureBoxes
+        Dim arrSeconds1 As PictureBox() = {picSecond1_1, picSecond1_2}
+        Dim arrSeconds2 As PictureBox() = {picSecond2_1, picSecond2_2, picSecond2_4, picSecond2_8}
+        Array.Reverse(arrSeconds1)
+        Array.Reverse(arrSeconds2)
+
+        'Updates the PictureBoxes representing the ones place
+        For Each time1 As PictureBox In arrSeconds1
+            Dim time As Integer = Convert.ToInt16(time1.Name.Substring(time1.Name.Length - 1, 1))
+            If time <= second1 Then
+                time1.Image = My.Resources.LightOn
+                second1 -= time
+            Else
+                time1.Image = My.Resources.LightOff
+            End If
+        Next
+
+        'Updates the PictureBoxes representing the tens place
+        For Each time2 As PictureBox In arrSeconds2
+            Dim time As Integer = Convert.ToInt16(time2.Name.Substring(time2.Name.Length - 1, 1))
+            If time <= second2 Then
+                time2.Image = My.Resources.LightOn
+                second2 -= time
+            Else
+                time2.Image = My.Resources.LightOff
+            End If
+        Next
+    End Sub
+End Class
